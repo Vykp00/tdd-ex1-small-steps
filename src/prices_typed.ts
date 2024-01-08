b2286362894efeecd64f3b613a50b28183e99f9f
Vykp00 <vy.le@edu.karelia.fi>
2024-01-08 10:56:19 +0200

    Parallel Change: Change isHoliday

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -78,7 +78,7 @@ function createApp(database: Database) {
 
   function calculateReduction(date: Date | undefined, date2: Temporal.PlainDate | undefined) {
     let reduction = 0;
-    if (date && isMonday(date, date2) && !isHoliday(date)) {
+    if (date && isMonday(date, date2) && !isHoliday(date, date2)) {
       reduction = 35;
     }
     return reduction;
@@ -88,15 +88,13 @@ function createApp(database: Database) {
     return date2?.dayOfWeek === 1;
   }
 
-  function isHoliday(date: Date | undefined) {
+  function isHoliday(date: Date | undefined, date2: Temporal.PlainDate | undefined) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
-        date &&
-        date.getFullYear() === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() &&
-        date.getDate() === holiday.getDate()
+        date2 && date2.equals(holiday2)
       ) {
         return true;
       }
